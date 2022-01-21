47f2287e6d203568876e2c21b575c8d820496aed
JackyLin1 <jlin9702@gmail.com>
2022-01-21 15:02:40 +0900

    some refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,26 @@
-const arg = process.argv.slice(2);
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  
 
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (response.statusCode !== 200 && body === null) {
+      console.log('error:', error)
+      return;
+    }
+    
+    if (body === '[]') {
+      console.log('This breed does not exist try another one');
+      return;
+    }
+    
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+      
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-  
-  if (response.statusCode !== 200 && body === null) {
-    console.log(error);
-    return;
-  }
-  
-  if (body === '[]') {
-    console.log('This breed does not exist try another one');
-    return;
-  }
-  
-  if (!error && response.statusCode == 200) {
-    const data = JSON.parse(body);
-    console.log(data[0]);
-    return;
-  }
-});
+module.exports = {fetchBreedDescription};
